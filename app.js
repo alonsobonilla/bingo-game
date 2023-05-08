7b68d7249466c10cbf384b2dbc21e252c8ce54da
Alonso Bonilla <luis.labp06@gmail.com>
2023-05-07 21:44:10 -0500

    Otra forma de manejar la aleatoriedad al momento de 'sacar' bolillas

--- app.js
+++ app.js
@@ -1,73 +1,80 @@
-const bolillasSacadas = new Array(75);
-const letrasBingo = ['B','I','N','G','O']
-const cambios = [1,16,31,46,61]
+//Variables
+const bolillas = [];
+const letrasBingo = ["B", "I", "N", "G", "O"];
+const cambios = [1, 16, 31, 46, 61];
+let variante = 74;
 
-const nuevaBolilla = document.querySelector('.agregar-bolilla')
-const reiniciar = document.querySelector('.reiniciar')
+const parrafoBolilla = document.querySelector(".text-bolilla");
+const nuevaBolilla = document.querySelector(".agregar-bolilla");
+const reiniciar = document.querySelector(".reiniciar");
 
-const parrafoBolilla = document.querySelector('.text-bolilla')
-
+//Renderiza los elementos al DOM
 showBolillas();
+//Llena el arreglo de bolillas
+fillArrayRandomsNums();
+
 //Eventos
-nuevaBolilla.addEventListener('click', iniciarJuego)
-reiniciar.addEventListener('click', reiniciarBolillas)
+nuevaBolilla.addEventListener("click", initGame);
+reiniciar.addEventListener("click", resetBolillas);
 
-function iniciarJuego() {
-    if(bolillasSacadas.length == 75) {
-        reiniciarBolillas()
-        return
-    }
-    const random = numeroRandom();
+function initGame() {
+  if (bolillas.length == 0) {
+    resetBolillas();
+    return;
+  }
 
-    const div = document.querySelector('.content-bolillas .b-' + random)
-    
-    parrafoBolilla.textContent = random
-    console.log(div)
-    div.classList.add('select-bolilla')
-
-    bolillasSacadas.push(random)
+  const bolilla = outBolilla();
+  const div = document.querySelector(".content-bolillas .b-" + bolilla);
+  parrafoBolilla.textContent = bolilla;
+  div.classList.add("select-bolilla");
 }
 
-function reiniciarBolillas() {
-    const bolillas = document.querySelectorAll('.content-bolillas div')
-    console.log(bolillas)
-    bolillas.forEach(bolilla => {
-        bolilla.classList.remove('select-bolilla')
-    })
-    parrafoBolilla.textContent = ''
-    //Borramos los elementos del arreglo
-    bolillasSacadas.splice(0, bolillasSacadas.length)
+function resetBolillas() {
+  const bolillas = document.querySelectorAll(".content-bolillas div");
+
+  bolillas.forEach((bolilla) => {
+    bolilla.classList.remove("select-bolilla");
+  });
+  parrafoBolilla.textContent = "";
+
+  fillArrayRandomsNums();
 }
 function showBolillas() {
-    const contentBolillas = document.querySelector('.content-bolillas')
-    
-    for(let i=0; i<5; i++) {
-        for(let j=cambios[i]; j<cambios[i]+15; j++) {
-            const div = document.createElement('DIV')
-            const parrafo = document.createElement('p')
-            if(j == cambios[i]) {
-                const div = document.createElement('DIV')
-                const parrafo = document.createElement('p')
-                div.classList.add(letrasBingo[i])
-                parrafo.textContent = letrasBingo[i]
-                div.appendChild(parrafo)
-                contentBolillas.appendChild(div)
-            }
-            div.classList.add('b-'+j)
-            parrafo.textContent = j
-    
-            div.appendChild(parrafo)
-            contentBolillas.appendChild(div)
-        }
+  const contentBolillas = document.querySelector(".content-bolillas");
+
+  for (let i = 0; i < 5; i++) {
+    for (let j = cambios[i]; j < cambios[i] + 15; j++) {
+      if (j == cambios[i]) {
+        const div = document.createElement("DIV");
+        const parrafo = document.createElement("p");
+        div.classList.add(letrasBingo[i]);
+        parrafo.textContent = letrasBingo[i];
+        div.appendChild(parrafo);
+        contentBolillas.appendChild(div);
+      }
+      const div = document.createElement("DIV");
+      const parrafo = document.createElement("p");
+      div.classList.add("b-" + j);
+      parrafo.textContent = j;
+
+      div.appendChild(parrafo);
+      contentBolillas.appendChild(div);
     }
+  }
 }
 
-function numeroRandom() {
-    let numeroRandom = Math.floor((Math.random() * (75 - 1 + 1)) + 1)
+function outBolilla() {
+  const posicion = Math.floor(Math.random() * variante);
 
-    while(bolillasSacadas.includes(numeroRandom)) {
-        numeroRandom = Math.floor((Math.random() * (75 - 1 + 1)) + 1)
-    }
+  const bolilla = bolillas[posicion];
+  bolillas.splice(posicion, 1);
+  variante--;
 
-    return numeroRandom;
+  return bolilla;
+}
+
+function fillArrayRandomsNums() {
+  for (let i = 0; i < 75; i++) {
+    bolillas[i] = i + 1;
+  }
 }
